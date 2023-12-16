e1a07df5ccd8304e4659e21b41b4ba65be68acbd
ckab04 <ciceronkapenabm@gmail.com>
2023-12-16 16:17:22 +0100

    my second stage (test1)

--- src/blob.rs
+++ src/blob.rs
@@ -7,13 +7,9 @@ pub fn read_blob(content_obj: DirEntry, arg: &String) -> String{
     let content_folder = fs::read_dir(v).unwrap();
     let val: Vec<String> = content_folder.into_iter().map(|x | {
         let v = x.unwrap().path().to_str().unwrap().to_string();
-        println!("Value in the slice {v}");
         let v = &v[13..];
-        println!("After slice  {v}");
         v.replace(&['/'], "").to_string()
     }).collect();
-
-    println!("Nothing ??? {:?}", val);
 
     let result = val.get(0);
     if result.is_some() && (result.unwrap() == arg)
