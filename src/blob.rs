f0b922b27a32f870c391533d84004a2e985c9aa6
ckab04 <ciceronkapenabm@gmail.com>
2023-12-16 14:20:46 +0100

    my second stage

---
+++ src/blob.rs
@@ -0,0 +1,8 @@
+
+
+pub fn read_blob(){
+
+
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod blob;
+
 #[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
@@ -15,7 +17,20 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
         println!("Initialized git directory")
-    } else {
+    } else if args[1] == "cat-file -p"{
+       let dir_content = fs::read_dir(".git/objects").unwrap();
+
+        for p in dir_content{
+            let v = p.unwrap().path().to_str().unwrap();
+            let file = v.replace(&['/', '.'], v);
+            if args[2].is_empty() && args[2] == file{
+                println!("{file}");
+                break;
+            }
+        }
+    }
+
+    else {
         println!("unknown command: {}", args[1])
     }
 }
