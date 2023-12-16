44279dea053bde8c3d6cf738bb5194577e210128
ckab04 <ciceronkapenabm@gmail.com>
2023-12-16 15:50:28 +0100

    my second stage 4

--- src/blob.rs
+++ src/blob.rs
@@ -3,10 +3,8 @@ use std::fs::DirEntry;
 
 // The return is the full file name (including the directory name)
 pub fn read_blob(content_obj: DirEntry, arg: &String) -> String{
-    let v = content_obj.path().to_str().unwrap();
-
+    let v = content_obj.path().to_str().unwrap().to_string();
     let content_folder = fs::read_dir(v).unwrap();
-
     let val: Vec<String> = content_folder.into_iter().map(|x | {
         let v = x.unwrap().path().to_str().unwrap().to_string();
         v.replace(&['/', '.'], "").to_string()
