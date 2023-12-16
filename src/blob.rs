27f590ef13baa5d2e9fd3a14dae4b0922c9e02ac
ckab04 <ciceronkapenabm@gmail.com>
2023-12-16 21:01:58 +0100

    Stage 3  (test1: init)

--- src/blob.rs
+++ src/blob.rs
@@ -1,5 +1,8 @@
 use std::fs;
 use std::fs::DirEntry;
+use std::io::Write;
+use flate2::Compression;
+use flate2::read::ZlibEncoder;
 
 // The return is the full file name (including the directory name)
 pub fn read_blob(content_obj: DirEntry, arg: &String) -> String{
@@ -19,3 +22,11 @@ pub fn read_blob(content_obj: DirEntry, arg: &String) -> String{
     String::new()
 
    }
+
+pub fn create_blob_object(file_name: &str){
+    let content = fs::read_to_string(file_name).expect("Failed to read the file");
+    let mut comp = ZlibEncoder::new(Vec::new(), Compression::default());
+    comp.write_all(content.as_bytes()).expect("Failed to compress");
+    let compressed = comp.finish().unwrap();
+    println!("{:?}", compressed);
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs;
 use std::process::Command;
 use std::str::from_utf8;
 use std::io::Write;
+use crate::blob::create_blob_object;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,31 +15,44 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else if args[1] == "cat-file" {
-        let dir_content = fs::read_dir(".git/objects").unwrap();
 
-        for p in dir_content {
-            let string = blob::read_blob(p.unwrap(), &args[3]);
-            if !string.is_empty() {
-                let res = Command::new("git")
-                    .arg("cat-file")
-                    .arg("-p")
-                    .arg(string)
-                    .output()
-                    .expect("Failed to execute the command");
-
-
-                write!(std::io::stdout(), "{}", from_utf8(&res.stdout[..]).unwrap()).expect("Failed to write to stdout");
-            }
-        }
+    match args[1].as_str() {
+        "init" => init(),
+        "cat-file" => cat_file(&args[3]),
+        "hash-object" => create_blob_object(&args[3]),
+        _ => println!("unknown command: {}", args[1])
     }
-    else {
-        println!("unknown command: {}", args[1])
+    // if args[1] == "init" {
+    //    init();
+    // } else if args[1] == "cat-file" {
+    //     cat_file( &args[3]);
+    // }
+    // else {
+    //     println!("unknown command: {}", args[1])
+    // }
+}
+fn init(){
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+    println!("Initialized git directory")
+}
+
+fn cat_file(cmd_line_arg: &String){
+    let dir_content = fs::read_dir(".git/objects").unwrap();
+
+    for p in dir_content {
+        let string = blob::read_blob(p.unwrap(), cmd_line_arg);
+        if !string.is_empty() {
+            let res = Command::new("git")
+                .arg("cat-file")
+                .arg("-p")
+                .arg(string)
+                .output()
+                .expect("Failed to execute the command");
+
+            write!(std::io::stdout(), "{}", from_utf8(&res.stdout[..]).unwrap()).expect("Failed to write to stdout");
+        }
     }
 }
