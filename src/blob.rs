bae59457826122a5b15bc15249d7a2036b04624c
ckab04 <ciceronkapenabm@gmail.com>
2023-12-16 15:49:29 +0100

    my second stage 4

--- src/blob.rs
+++ src/blob.rs
@@ -8,15 +8,14 @@ pub fn read_blob(content_obj: DirEntry, arg: &String) -> String{
     let content_folder = fs::read_dir(v).unwrap();
 
     let val: Vec<String> = content_folder.into_iter().map(|x | {
-        let v = x.unwrap().path().to_str().unwrap();
+        let v = x.unwrap().path().to_str().unwrap().to_string();
         v.replace(&['/', '.'], "").to_string()
     }).collect();
 
-    if val.is_empty(){
-        return String::new();
-    }
-
     let result = val.get(0).expect("Unable to get the path");
-    result.into()
+    if result == arg{
+        return result.to_string();
+    }
+    String::new()
 
    }
